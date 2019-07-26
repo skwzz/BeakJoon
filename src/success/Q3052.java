7f4e19aaeefae00b41bd601402887d1dae6d6962
skw <kidol9108@gmail.com>
2019-07-26 10:36:49 +0900

    나머지

---
+++ src/success/Q3052.java
@@ -0,0 +1,31 @@
+package success;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q3052 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[10];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine()) % 42;
+		}
+		int cnt=1;
+		for(int i=0; i<arr.length-1; i++) {
+			boolean check = false;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					check = true;
+					break;
+				}
+			}
+			if(!check) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+		
+	}
+}
