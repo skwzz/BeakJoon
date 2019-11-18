7cbbe28ef99fdae440741f42eb039d1f35ca9ba4
skwzz <kidol9108@gmail.com>
2019-11-18 14:49:30 +0900

    boj

---
+++ src/success/Q2446.java
@@ -0,0 +1,43 @@
+package success;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2446 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		int temp = 0;
+		int blank = n-1;
+		boolean goDown = true;
+		
+		while(temp<2*n-1) {
+			
+			for(int i=1; i<n-blank; i++) {
+				sb.append(" ");
+			}
+			for(int i=0; i<2*(blank+1)-1; i++) {
+				sb.append("*");
+			}
+			
+			
+			if(goDown) {
+				blank--;
+			}else {
+				blank++;
+			}
+			
+			if(blank==0) {
+				goDown=false;
+			}
+			temp++;
+			if(temp<2*n-1) {
+				sb.append("\n");
+			}
+		}
+		System.out.print(sb.toString());
+	}
+}
--- src/success/Q2522.java
+++ src/success/Q2522.java
@@ -1,13 +1,13 @@
+package success;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q2446 {
+public class Q2522 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
-		
 		
 		int temp = 0;
 		int blank = n-1;
@@ -18,12 +18,7 @@ public class Q2446 {
 			for(int i=n; i>blank; i--) {
 				sb.append("*");
 			}
-			for(int i=0; i<blank*2; i++) {
-				sb.append(" ");
-			}
-			for(int i=n; i>blank; i--) {
-				sb.append("*");
-			}
+						
 			
 			if(goDown) {
 				blank--;
@@ -35,7 +30,9 @@ public class Q2446 {
 				goDown=false;
 			}
 			temp++;
-			sb.append("\n");
+			if(temp<2*n-1) {
+				sb.append("\n");
+			}
 		}
 		System.out.print(sb.toString());
 	}
